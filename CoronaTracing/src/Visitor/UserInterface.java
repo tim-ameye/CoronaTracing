913b73af87073901941d42c93acca9de2c1ddeaa
PieterCarlu <pieter.carlu@telenet.be>
2020-12-01 18:52:18 +0100

    userinterface

---
+++ CoronaTracing/src/Visitor/UserInterface.java
@@ -0,0 +1,27 @@
+package Visitor;
+
+import java.awt.GridBagLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.Insets;
+import java.awt.event.*;
+import java.util.*;
+import java.awt.Font;
+import java.awt.EventQueue;
+
+import javax.swing.border.EmptyBorder;
+
+
+
+public class UserInterface extends JFrame {
+
+}
--- CoronaTracing/src/Visitor/VisitorInterface.java
+++ CoronaTracing/src/Visitor/VisitorInterface.java
@@ -1,8 +1,15 @@
 package Visitor;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.*;
 
 public interface VisitorInterface extends Remote {
 	public void testConnection(String s) throws RemoteException;
+	//public void tell(String s) throws RemoteException;
+	//public void setGUI(UserInterface t) throws RemoteException;
+	//public UserInterface getGUI() throws RemoteException;
+	//public void updateUI(Vector v) throws RemoteException;
+	 
+	
 
 }

0154622559fb2822a1702f9305e47466e1c13a13
PieterCarlu <pieter.carlu@telenet.be>
2020-12-01 18:25:53 +0100

    visitor connectie werkt

--- CoronaTracing/src/Visitor/VisitorClient.java
+++ CoronaTracing/src/Visitor/VisitorClient.java
@@ -1,5 +1,40 @@
 package Visitor;
 
-public class VisitorClient {
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Scanner;
 
+import cateringFacility.CateringFacility;
+import cateringFacility.RegistrarInterface;
+
+public class VisitorClient {
+	public static void main(String[] args) {
+		RegistrarInterface server;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Please enter first name : ");
+		String firstName = sc.nextLine();
+		System.out.println("Please enter last name : ");
+		String lastName = sc.nextLine();
+		System.out.println("Please enter phoneNumber: ");
+		String phoneNumber = sc.nextLine();
+		
+		try {
+			Visitor visitor = new Visitor(firstName, lastName, phoneNumber);
+			visitor.testConnection("Test from visitorside");
+			
+			Registry myRegistry = LocateRegistry.getRegistry("localhost", 55545);
+			server = (RegistrarInterface) myRegistry.lookup("Registrar"); 
+			
+			server.registerVisitor(visitor);
+			
+			
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
+	}
 }

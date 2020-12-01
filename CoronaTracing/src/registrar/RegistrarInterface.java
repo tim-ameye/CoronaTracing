d9539b0d19eb630a1aaed5aed2552017dee2e8e2
timam <tim.ameye@student.kuleuven.be>
2020-12-01 16:17:21 +0100

    Registrar

--- CoronaTracing/src/registrar/RegistrarInterface.java
+++ CoronaTracing/src/registrar/RegistrarInterface.java
@@ -1,5 +1,13 @@
 package registrar;
 
+import java.rmi.RemoteException;
+
 public interface RegistrarInterface {
 
+	public boolean registerCateringFacility() throws RemoteException; 	//TODO CF-interface opvragen
+	public boolean loginCF() throws RemoteException;
+	
+	
+	public boolean registerUser() throws RemoteException;				//TODO User-interface opvragen
+	public boolean loginUser() throws RemoteException;
 }
--- CoronaTracing/src/registrar/User.java
+++ CoronaTracing/src/registrar/User.java
@@ -1,14 +1,55 @@
 package registrar;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Date;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
 public class User {
 
 	private String name;
 	private String surname;
 	private String phoneNumber;	//unique identifier
 	
+	private SecretKey secretKey;
+	
 	public User(String name, String surname, String phonNumber) {
 		this.name = name;
 		this.surname = surname;
 		this.phoneNumber = phonNumber;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public void generateTokenSet(int size) {
+		for(int i = 0; i < size; i++) {
+			SecureRandom random = new SecureRandom();
+			
+		}
+	}
+	
 }

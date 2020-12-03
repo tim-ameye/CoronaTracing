76d55e992c67abf7ea986de0521b02ad41c9f3dc
PieterCarlu <pieter.carlu@telenet.be>
2020-12-03 16:11:02 +0100

    gui voor de visitor

---
+++ CoronaTracing/src/Visitor/guiVisitor.java
@@ -0,0 +1,162 @@
+package Visitor;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class guiVisitor {
+
+	private JFrame frame;
+	private JTextField textField_firstName;
+	private JTextField textField_lastName;
+	private JTextField textField_phoneNumber;
+	private JTextField textField_QRinputstring;
+	private JPanel panelLogin;
+	private JPanel panelScan;
+	private JPanel panelSucces;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					guiVisitor window = new guiVisitor();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public guiVisitor() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 * gui gemaakt met https://www.youtube.com/watch?v=bKPGEqJHWaE
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 400, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new CardLayout(0, 0));
+		
+		JPanel panelLogin = new JPanel();
+		frame.getContentPane().add(panelLogin, "name_861651163587100");
+		panelLogin.setLayout(null);
+		panelLogin.setVisible(true);
+		
+		JPanel panelScan = new JPanel();
+		frame.getContentPane().add(panelScan, "name_861655137015600");
+		panelScan.setLayout(null);
+		panelScan.setVisible(false);
+		
+		JPanel panelSucces = new JPanel();
+		frame.getContentPane().add(panelSucces, "name_861659305217200");
+		panelSucces.setLayout(null);
+		panelSucces.setVisible(false);
+		
+		JLabel lblNewLabel = new JLabel("First Name:");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Verdana", Font.PLAIN, 18));
+		lblNewLabel.setBounds(0, 80, 384, 30);
+		panelLogin.add(lblNewLabel);
+		
+		JLabel lblLastName = new JLabel("Last Name:");
+		lblLastName.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLastName.setFont(new Font("Verdana", Font.PLAIN, 18));
+		lblLastName.setBounds(0, 160, 384, 30);
+		panelLogin.add(lblLastName);
+		
+		JLabel lblPhonenumber = new JLabel("Phonenumber:");
+		lblPhonenumber.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPhonenumber.setFont(new Font("Verdana", Font.PLAIN, 18));
+		lblPhonenumber.setBounds(0, 240, 384, 30);
+		panelLogin.add(lblPhonenumber);
+		
+		textField_firstName = new JTextField();
+		textField_firstName.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_firstName.setBounds(125, 120, 150, 30);
+		panelLogin.add(textField_firstName);
+		textField_firstName.setColumns(10);
+		
+		textField_lastName = new JTextField();
+		textField_lastName.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_lastName.setColumns(10);
+		textField_lastName.setBounds(125, 200, 150, 30);
+		panelLogin.add(textField_lastName);
+		
+		textField_phoneNumber = new JTextField();
+		textField_phoneNumber.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_phoneNumber.setColumns(10);
+		textField_phoneNumber.setBounds(125, 280, 150, 30);
+		panelLogin.add(textField_phoneNumber);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//TODO send gegevens naar een databank
+				panelScan.setVisible(true);
+				panelLogin.setVisible(false);
+			}
+		});
+		btnLogin.setFont(new Font("Verdana", Font.PLAIN, 20));
+		btnLogin.setBounds(100, 350, 200, 50);
+		panelLogin.add(btnLogin);
+		
+		JButton btnScanQR = new JButton("Scan!");
+		btnScanQR.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//TODO controleren of die wel goed wordt gegenereerd
+				panelSucces.setVisible(true);
+				panelScan.setVisible(false);
+			}
+		});
+		btnScanQR.setFont(new Font("Verdana", Font.PLAIN, 20));
+		btnScanQR.setBounds(100, 350, 200, 50);
+		panelScan.add(btnScanQR);
+		
+		JLabel lblPleaseScanThe = new JLabel("Please scan the QR-code");
+		lblPleaseScanThe.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPleaseScanThe.setFont(new Font("Verdana", Font.PLAIN, 18));
+		lblPleaseScanThe.setBounds(0, 80, 384, 30);
+		panelScan.add(lblPleaseScanThe);
+		
+		textField_QRinputstring = new JTextField();
+		textField_QRinputstring.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_QRinputstring.setBounds(100, 120, 200, 200);
+		panelScan.add(textField_QRinputstring);
+		textField_QRinputstring.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("You have succesfully scanned the QR-code! ");
+		lblNewLabel_1.setFont(new Font("Verdana", Font.PLAIN, 16));
+		lblNewLabel_1.setBounds(10, 160, 364, 30);
+		panelSucces.add(lblNewLabel_1);
+		
+		JLabel lblCheers = new JLabel("Cheers!");
+		lblCheers.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCheers.setFont(new Font("Verdana", Font.BOLD, 25));
+		lblCheers.setForeground(new Color(0, 0, 0));
+		lblCheers.setBounds(95, 180, 200, 100);
+		panelSucces.add(lblCheers);
+	}
+}

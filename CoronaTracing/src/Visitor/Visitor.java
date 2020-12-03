b91ee00ab9f3b78a870bc68299e9bf81241906a9
PieterCarlu <pieter.carlu@telenet.be>
2020-12-03 17:53:16 +0100

    denk dat zo is

--- CoronaTracing/src/Visitor/Visitor.java
+++ CoronaTracing/src/Visitor/Visitor.java
@@ -4,7 +4,7 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Vector;
 
-public class Visitor extends UnicastRemoteObject implements VisitorInterface{
+public class Visitor{
 	private String firstName;
 	private String lastName;
 	private String phoneNumber;
@@ -15,11 +15,6 @@ public class Visitor extends UnicastRemoteObject implements VisitorInterface{
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.phoneNumber = phoneNumber;
-	}
-	
-	@Override
-	public void testConnection(String s) throws RemoteException{
-		System.out.println("test "+s);
 	}
 	
 	public String getFirstName() {
@@ -44,33 +39,7 @@ public class Visitor extends UnicastRemoteObject implements VisitorInterface{
 	
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
-	}
-
-	@Override
-	public void alreadyRegistered() throws RemoteException {
-		System.out.println("Already registered, login to continue.");
-		
-	}
-
-	@Override
-	public void setGUI(guiVisitor t) throws RemoteException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public guiVisitor getGUI() throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void updateUI(Vector v) throws RemoteException {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
+	}	
 	
 	
 }

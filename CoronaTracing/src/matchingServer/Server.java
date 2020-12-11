febc5f9d4051a9d8598224ea2ef444c526fdebee
victorGoeman <victorgoeman@hotmail.com>
2020-12-11 14:07:14 +0100

    quick fix

--- CoronaTracing/src/matchingServer/Server.java
+++ CoronaTracing/src/matchingServer/Server.java
@@ -17,27 +17,27 @@ public class Server {
 		
 		Scanner sc = new Scanner(System.in);
 		Registry registry = null;
-		String dbFile = "files\\MatchingService.txt";
-		Database db = new Database(dbFile);
+		
+		String dbFile = "\\MachingService\\Database.txt";
+		
 		
 		String input = "";
 		while(!input.equals("Stop")) {
 			if(input.equals("Start")) {
 				System.out.println("[MatchingService] The database has been initialised.");
 				
-					db.readFile();
 				
 				
 				try {
 					registry = LocateRegistry.createRegistry(55547);
-					registry.rebind("MatchingService", new MatchingService(db));
+					registry.rebind("MatchingService", new MatchingService());
+					
 				} catch (Exception e) {
 					System.out.println("[MatchingService] System has failed to start: "+ e.getLocalizedMessage());
 					e.printStackTrace();
 				}
 			} else if(input.equals("Save")) {
-				db.printFile();
-				System.out.println("[MatchingService] The database has been updated.");
+				System.out.println("[MatchingService] The database has been updated, jk this command does absolutly noting.");
 			} else if(input.equals("Expired")) {
 				System.out.println("[MatchingService] Out of date hashes and tokens have been removed. Save to put the changes in the files.");
 			}

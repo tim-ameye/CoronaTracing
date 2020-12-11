feab6706079375c2f8a1814cbdfbc34f29d2b29c
PieterCarlu <pieter.carlu@telenet.be>
2020-12-11 19:23:27 +0100

    merge weg

--- CoronaTracing/src/matchingServer/MatchingServiceInterface.java
+++ CoronaTracing/src/matchingServer/MatchingServiceInterface.java
@@ -3,17 +3,17 @@ package matchingServer;
 import java.io.FileNotFoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-<<<<<<< HEAD
+
 
 import mixingProxy.Capsule;
 
-=======
+
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 import Visitor.Visit;
 import mixingProxy.Capsule;
->>>>>>> stash
+
 
 public interface MatchingServiceInterface extends Remote{
 	public List<String> getCriticalRecordsOfToday() throws RemoteException;

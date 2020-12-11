28a580af5f625e354cfd46bc11fde2f94c4603eb
timam <tim.ameye@student.kuleuven.be>
2020-12-11 18:08:02 +0100

    Every halfour, the capsules are send to the matching server

--- CoronaTracing/src/mixingProxy/Server.java
+++ CoronaTracing/src/mixingProxy/Server.java
@@ -15,13 +15,18 @@ public class Server {
 		
 		try {
 			Registry registry = LocateRegistry.createRegistry(55546);
-			
-			registry.rebind("MixingProxy", new MixingProxy());
+			MixingProxy mixingProxy  = new MixingProxy();
+			registry.rebind("MixingProxy", mixingProxy);
 			Scanner sc = new Scanner(System.in);
 			String input = "";
+			long currentTime = System.currentTimeMillis();
+			long lastTime = System.currentTimeMillis();
 			while(!input.equals("Stop")) {
 				if(sc.hasNext()) {
 					input = sc.nextLine();
+				}
+				if(currentTime > lastTime + 18000000) { //ieder halfuur
+					mixingProxy.sendCapsules();
 				}
 			}
 			registry.unbind("MixingProxy");
